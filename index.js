86faac7f10b3d931c7b1d7ac73641eee950bed88
Danny Castonguay <d@bld.ai>
2023-10-05 18:27:55 +1100

    Update main branch

--- index.js
+++ index.js
@@ -3,32 +3,52 @@ import init, { route_command } from '../pkg/epsilon_academy.js';
 async function run() {
   await init();
 
+  const history = [];
+  let historyIndex = -1;
+
   function executeCommand() {
     const terminal = document.getElementById('terminal');
     const inputElement = document.getElementById('commandInput');
     const input = inputElement.value;
-
+    history.push(input);
+    historyIndex = history.length;
     const [command, ...params] = input.split(' ');
 
-    // Append the command to terminal
     const commandElement = document.createElement('div');
     commandElement.textContent = '> ' + input;
     terminal.appendChild(commandElement);
 
-    // Append the output to terminal
     const output = route_command(command, params.join(' '));
     const outputElement = document.createElement('div');
     outputElement.textContent = output;
     terminal.appendChild(outputElement);
-
-    // Clear the input
     inputElement.value = '';
   }
 
+  const inputElement = document.getElementById('commandInput');
 
-  document.getElementById('commandInput').addEventListener('keydown', function(event) {
+  inputElement.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
       executeCommand();
+    }
+
+    if (event.key === 'ArrowUp' && historyIndex > 0) {
+      event.preventDefault();
+      historyIndex--;
+      inputElement.value = history[historyIndex];
+      inputElement.setSelectionRange(inputElement.value.length, inputElement.value.length);
+    }
+
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      if (historyIndex < history.length - 1) {
+        historyIndex++;
+        inputElement.value = history[historyIndex];
+      } else if (historyIndex === history.length - 1) {
+        historyIndex++;
+        inputElement.value = '';
+      }
+      inputElement.setSelectionRange(inputElement.value.length, inputElement.value.length);
     }
   });
 
