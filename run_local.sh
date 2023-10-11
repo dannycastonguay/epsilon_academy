23da808a5c405cf3ee802d45da2d3b1d9c7309c7
Danny Castonguay <d@bld.ai>
2023-10-11 09:04:31 +0800

    Update main branch

--- run_local.sh
+++ run_local.sh
@@ -3,5 +3,9 @@
 # Compile the project to WebAssembly
 wasm-pack build --target web
 
+# Open the default web browser to navigate to the local server
+open 'http://localhost:8000'
+
 # Run a local server
 python3 -m http.server
+
