845d59be49bcb26c340797078e706e9a76ce3b87
pascalecapitancastonguay <40507640+pascalecapitancastonguay@users.noreply.github.com>
2023-10-12 01:26:45 +0000

    create dragon

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Define utility functions
+function dragon () {
+  return "You have been reincarnated as a dragon are you a boy or girl?"
+}
+
 function about() {
   return "The purpose of this web app is to run simple applications. Epsilon in math is a small positive number, and in our context, it symbolizes infinite curiosity. Founders: Danny Castonguay (your bio here), Pascale (Inspired by French math), Linus (Inspired by the creator of Linux), Ada (Inspired by the first programmer). We design all software products ourselves and publish content on what we learn. Try the following commands: about (this), hello_world, and prime_numbers NUM.";
 }
