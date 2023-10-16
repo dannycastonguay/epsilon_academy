5ceca3b51a797f2130fbf045e59a2d30c7806ab6
Danny Castonguay <d@bld.ai>
2023-10-16 17:39:18 +0800

    dragon story fixed

--- apps/dragon.js
+++ apps/dragon.js
@@ -2,54 +2,47 @@ import { ask } from '../common.js';
 
 export async function dragon() {
   const gender = await ask("You have been reincarnated as a dragon. Are you a [boy], a [girl], or something_else?");
-  const dragonType = (await ask(`Okay, you are a ${gender}. Are you a [Seawing], [Rainwing], [Nightwing], [Mudwing], [Sandwing], or [Skywing]?`)).toLowerCase();
 
-  let description;
+  let happy = "no"
+  let dragonType;
 
-  switch (dragonType) {
-    case "seawing":
-      description = "A seawing is a dragon that can breathe underwater, they have glowing scales to communicate.";
-      break;
-    case "rainwing":
-      description = "A rainwing has color-changing scales, and can spit venom. But they have to have their suntime during the day.";
-      break;
-    case "nightwing":
-      description = "Nightwings are nocturnal dragons, they are black to mimic the night sky with stars below their wings. And can breathe fire.";
-      break;
-    case "mudwing":
-      description = "Mudwings are large and bulky dragons, they like to stay in a pack with their siblings and can breathe underwater for 2 hours.";
-      break;
-    case "sandwing":
-      description = "These dragons are specially accustomed to the desert, they have a poisonous barb on their tail and their scales generate warmth.";
-      break;
-    case "skywing":
-      description = "Skywings are tall dragons with large wings, they are the fastest flying dragons, they can breathe fire.";
-      break;
-    default:
-      description = `What's a ${dragonType}? You aren't even a dragon!!`;
-  }
+  while(happy !== "yes") {
+    dragonType = (await ask(`Okay, you are a ${gender}. Are you a [Seawing], [Rainwing], [Nightwing], [Mudwing], [Sandwing], or [Skywing]?`)).toLowerCase();
 
-  alert("Calling story now!");  // Moved out of the switch
-  const result = await story(gender, dragonType);  // Moved out of the switch
+    let description;
 
-  const option = await ask(`You have chosen to be a ${dragonType}.\n${description}\nAre you sure you want to be this dragon [yes] or [no]`);
-  if (option.toLowerCase() === "yes") {
-    console.log(result);
-  } else {
-    const newDragonType = (await ask("Are you a [Seawing], [Rainwing], [Nightwing], [Mudwing], [Sandwing], or [Skywing]?")).toLowerCase();
-    dragon(newDragonType); // Recursive call with the new choice
-  }
-}
-//nothing is working :(
-
-// @p: this is hilarious that you call story from inside dragon. There's a more elegant way to achieve your objectives
-async function story(gender, dragonType) {
-  const intro = (await ask(`You wake up as a ${gender} ${dragonType} dragon in your egg.What will you do?[1] Break out of the egg[2] Stay in the egg`)).trim();
-  alert("You chose: " + intro);
-
-
-    switch (intro) {
-      case "1":return"You burst out of your egg, floating in the water. Your mother picks you up into her arms.";
-      case "2":return"You decide it's too scary and wait inside the egg for a while.";
+    switch (dragonType) {
+      case "seawing":
+        description = "A seawing is a dragon that can breathe underwater, they have glowing scales to communicate.";
+        break;
+      case "rainwing":
+        description = "A rainwing has color-changing scales, and can spit venom. But they have to have their suntime during the day.";
+        break;
+      case "nightwing":
+        description = "Nightwings are nocturnal dragons, they are black to mimic the night sky with stars below their wings. And can breathe fire.";
+        break;
+      case "mudwing":
+        description = "Mudwings are large and bulky dragons, they like to stay in a pack with their siblings and can breathe underwater for 2 hours.";
+        break;
+      case "sandwing":
+        description = "These dragons are specially accustomed to the desert, they have a poisonous barb on their tail and their scales generate warmth.";
+        break;
+      case "skywing":
+        description = "Skywings are tall dragons with large wings, they are the fastest flying dragons, they can breathe fire.";
+        break;
+      default:
+        description = `What's a ${dragonType}? You aren't even a dragon!!`;
     }
+
+    happy = (await ask(`You have chosen to be a ${dragonType}.\n${description}\nAre you sure you want to be this dragon [yes] or [no]`)).toLowerCase();
+  }
+
+  const intro = (await ask(`You wake up as a ${gender} ${dragonType} dragon in your egg. What will you do? [1] Break out of the egg [2] Stay in the egg`));
+
+  switch (intro) {
+    case "1" : return "You burst out of your egg, floating in the water. Your mother picks you up into her arms. THE END.";
+    case "2" : return "You decide it's too scary and wait inside the egg for a while. THE END.";
+    default: return "That's not a story option. Bye!"
+  }
+
 }
