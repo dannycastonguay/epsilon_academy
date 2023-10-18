d98fbdcf94e593cff7734bd85fe3df853f7372fe
Danny Castonguay <dannycastonguay@users.noreply.github.com>
2023-10-18 21:43:49 +0800

    clean the repo

--- run_local.sh
+++ run_local.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Run a local server in the background
-python3 -m http.server &
-
-# Wait to make sure the server is up
-sleep 1
-
-# Open the default web browser to navigate to the local server
-open 'http://localhost:8000'
+# Run browser-sync to serve the files and watch for changes
+browser-sync start --server --files "." &
