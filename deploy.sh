3c9a520a03d0fb46495c8dd841ddac54576f2d20
Danny Castonguay <dannycastonguay@users.noreply.github.com>
2023-10-19 00:25:06 +0800

    Update main branch

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
-# Checkout to main branch and pull latest changes
+
+# Commit and push to main
 git checkout main
-git pull origin main
-
-# Minify main JavaScript file
-./node_modules/.bin/esbuild index.js --bundle --minify --outfile=index.min.js
-
-# Loop through and minify each JavaScript file in the apps directory
-for file in apps/*.js; do
-  ./node_modules/.bin/esbuild "$file" --bundle --minify --outfile="${file%.js}.min.js"
-done
-
-# Minify CSS
-./node_modules/.bin/esbuild styles.css --loader=css --minify --outfile=styles.min.css
-
-# Add and commit changes
-git add -A
-git commit -m "Minify JS and CSS for deployment"
-
-# Push changes to main branch
+git add .
+git commit -m "Update main branch"
 git push origin main
 
-# Checkout to gh-pages branch and pull the latest changes
+# Make gh-pages an exact copy of main
 git checkout gh-pages
-git pull origin gh-pages
+git reset --hard main
+git push -f origin gh-pages
 
-# Copy minified files to gh-pages branch
-cp index.min.js index.min.js
-cp styles.min.css styles.min.css
-
-# Copy minified app files to gh-pages/apps directory
-cp apps/*.min.js apps/
-
-# Add and commit changes
-git add -A
-git commit -m "Deploy minified JS and CSS to GitHub Pages"
-
-# Push changes to gh-pages branch
-git push origin gh-pages
-
-# Switch back to main branch
+# Switch back to main
 git checkout main
+
+# Open the default web browser to navigate to github
+open 'https://github.com/dannycastonguay/epsilon_academy'
