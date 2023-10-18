a6a48ac6236d2978e703d4d605cc4124f4b16376
Danny Castonguay <dannycastonguay@users.noreply.github.com>
2023-10-18 10:47:01 +0800

    Update main branch

--- apps/about.js
+++ apps/about.js
@@ -4,18 +4,17 @@ export async function about() {
   return `
 # Epsilon Academy: Ignite Your Infinite Curiosity
 
-Welcome to Epsilon Academy, where we aim to run **simple yet impactful applications**.
+Welcome to Epsilon Academy. Our focus is on creating educational software that aims to nurture an enduring passion for learning in individuals.
 
 ## The Name "Epsilon"
 
-In mathematics, Epsilon represents a *small positive number*, symbolic of the **infinite curiosity** we wish to spark in you.
-
+In mathematics, the term 'Epsilon' is used to denote a small positive number. We find this symbolism fitting for our mission. Our goal is to ignite a spark of curiosity that, while small at the onset, has the potential to grow infinitely over time.
 ## Founders
 
-- **Danny Castonguay**: (Your bio here)
-- **Pascale**: Inspired by the rigor of French mathematics.
-- **Linus**: Taking cues from Linus Torvalds, the creator of Linux.
-- **Ada**: Named after Ada Lovelace, the first programmer.
+- **Danny Castonguay: [resume](https://resume.dannycastonguay.com) and [blog](https://blog.dannycastonguay.com).**
+- **Pascale**: Named in honor of Blaise Pascal and Pierre de Fermat, whose groundbreaking work in probability theory laid the foundation for modern statistics and machine learning. Without their contributions, advanced technologies like GPT models would not be possible.
+- **Linus**: Inspired by Linus Torvalds, who exemplifies the profound impact one motivated individual can have. His work on Linux and Git underpins much of the modern internet, serving as an inspiration for our commitment to ambitious, collaborative projects.
+- **Ada**:  In homage to Ada Lovelace, the world's first programmer who worked with Charles Babbage on the Analytical Engine. As the daughter of the famed poet Lord Byron, Ada Lovelace brings a unique blend of arts and sciences, symbolizing our dedication to a diverse and interdisciplinary approach to technology.
 
 ## What We Do
 
